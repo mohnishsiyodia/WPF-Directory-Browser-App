99aabfde634cea41228c8ce83aae969ac4ce793c
Mohnish Siyodia <msiyodia96@gmail.com>
2024-09-10 23:00:04 +0530

    first commit

---
+++ WPF_Directory_Browser_App/Models/DatabaseHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyApp.Models
+{
+    public static class DatabaseHelper
+    {
+        private static string dbPath = "Images.db";
+
+        static DatabaseHelper()
+        {
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+                var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+                conn.Open();
+                string createTableQuery = "CREATE TABLE Images (Id INTEGER PRIMARY KEY AUTOINCREMENT, FileName TEXT, ImageData BLOB)";
+                var cmd = new SQLiteCommand(createTableQuery, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void SaveImage(string fileName, byte[] imageData)
+        {
+            var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            conn.Open();
+            string insertQuery = "INSERT INTO Images (FileName, ImageData) VALUES (@FileName, @ImageData)";
+            var cmd = new SQLiteCommand(insertQuery, conn);
+            cmd.Parameters.AddWithValue("@FileName", fileName);
+            cmd.Parameters.AddWithValue("@ImageData", imageData);
+            cmd.ExecuteNonQuery();
+        }
+
+        public static SQLiteDataReader GetImages()
+        {
+            var conn = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            conn.Open();
+            string selectQuery = "SELECT * FROM Images";
+            var cmd = new SQLiteCommand(selectQuery, conn);
+            return cmd.ExecuteReader();
+        }
+    }
+}
---
+++ WPF_Directory_Browser_App/ViewModels/DirectoryBrowserViewModel.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Win32;
+using MyApp.Models;
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyApp.ViewModels
+{
+    public class DirectoryBrowserViewModel : INotifyPropertyChanged
+    {
+        private string currentDirectory;
+        public string CurrentDirectory
+        {
+            get => currentDirectory;
+            set
+            {
+                currentDirectory = value;
+                OnPropertyChanged(nameof(CurrentDirectory));
+                LoadDirectoryContents();
+            }
+        }
+
+        public ObservableCollection<string> DirectoryContents { get; set; }
+        public ObservableCollection<ImageModel> UploadedImages { get; set; }
+
+        public ICommand BrowseCommand { get; }
+        public ICommand UploadCommand { get; }
+
+        public DirectoryBrowserViewModel()
+        {
+            DirectoryContents = new ObservableCollection<string>();
+            UploadedImages = new ObservableCollection<ImageModel>();
+            BrowseCommand = new RelayCommand(BrowseDirectory);
+            UploadCommand = new RelayCommand(UploadImage);
+            LoadUploadedImages();
+        }
+
+        private void BrowseDirectory(object parameter)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                CurrentDirectory = dialog.SelectedPath;
+            }
+        }
+
+        private void LoadDirectoryContents()
+        {
+            if (Directory.Exists(CurrentDirectory))
+            {
+                DirectoryContents.Clear();
+                foreach (var dir in Directory.GetDirectories(CurrentDirectory))
+                {
+                    DirectoryContents.Add(dir);
+                }
+                foreach (var file in Directory.GetFiles(CurrentDirectory))
+                {
+                    DirectoryContents.Add(file);
+                }
+            }
+        }
+
+        private void UploadImage(object parameter)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Image Files (*.png)|*.png"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                var fileName = dialog.FileName;
+                var imageData = File.ReadAllBytes(fileName);
+                DatabaseHelper.SaveImage(Path.GetFileName(fileName), imageData);
+                LoadUploadedImages();
+            }
+        }
+
+        private void LoadUploadedImages()
+        {
+            UploadedImages.Clear();
+            var reader = DatabaseHelper.GetImages();
+            while (reader.Read())
+            {
+                UploadedImages.Add(new ImageModel
+                {
+                    Id = reader.GetInt32(0),
+                    FileName = reader.GetString(1),
+                    ImageData = (byte[])reader.GetValue(2)
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
+
+
